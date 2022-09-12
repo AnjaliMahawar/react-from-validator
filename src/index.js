fd0584ece3940e195b062133dcb86d1c4f0f411e
Anjali MahawR <anjalimahawar454@gmail.com>
2022-09-12 05:48:03 +0530

    done

--- src/index.js
+++ src/index.js
@@ -6,11 +6,13 @@ import reportWebVitals from './reportWebVitals';
 import App5 from './App5';
 import App2 from './App2';
 import App6 from './App6';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import App7 from './App7';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App5 />
+    <App7 />
   </React.StrictMode>
 );
 
